894c213a4ae16325ceebdf047b2e7751c4ff05ee
aratnamirasugab <officialbagus15@gmail.com>
2019-09-28 00:48:30 +0700

    two sum solution reedit

--- interview/Leetcode - Two Sum.js
+++ interview/Leetcode - Two Sum.js
@@ -1,5 +1,4 @@
 var twoSum = function(nums, target) {
-
     for(let i = 0 ; i < nums.length ; i++) {
         for(let j = 0 ; j < nums.length ; j++){
             if(nums[i] + nums[j] === target && i !== j){
@@ -18,3 +17,22 @@ console.log(twoSum(nums, target));
 
 // Runtime: 152 ms, faster than 21.33% of JavaScript online submissions for Two Sum.
 // Memory Usage: 34.7 MB, less than 64.46% of JavaScript online submissions for Two Sum.
+
+// optimized solution 
+
+var twoSum = function(nums, target) {
+    var ans = [];
+    var exist = {};
+    
+    for (var i = 0; i < nums.length; i++){
+        if (typeof(exist[target-nums[i]]) !== 'undefined'){
+            ans.push(exist[target-nums[i]]);
+            ans.push(i);
+        }
+        exist[nums[i]] = i;
+    }
+    
+    return ans;
+};
+    
+    
