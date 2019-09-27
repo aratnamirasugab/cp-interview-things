af435af0e442a8e3e21df0b55a426e8615c45e2e
aratnamirasugab <officialbagus15@gmail.com>
2019-09-28 00:07:27 +0700

    two sum solution reedit

--- interview/Leetcode - Two Sum.js
+++ interview/Leetcode - Two Sum.js
@@ -14,3 +14,7 @@ let nums = [2,7,11,15];
 let target = 9;
 
 console.log(twoSum(nums, target));
+
+
+// Runtime: 152 ms, faster than 21.33% of JavaScript online submissions for Two Sum.
+// Memory Usage: 34.7 MB, less than 64.46% of JavaScript online submissions for Two Sum.
