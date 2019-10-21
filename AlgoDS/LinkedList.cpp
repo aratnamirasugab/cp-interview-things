c31abfb2c7b3d5c8410c72667febc0ef72bc2fe7
aratnamirasugab <officialbagus15@gmail.com>
2019-10-21 22:03:47 +0700

    added linkedlist & gitignore

--- AlgoDS/LinkedList.cpp
+++ AlgoDS/LinkedList.cpp
@@ -1,34 +1,83 @@
 #include <iostream>
+#include <cstdlib>
+#include <stdlib.h>
 
 using namespace std;
 
-class Node{
-public :
-    int data;
+struct Node{
+    int val;
     Node *next;
 };
 
-void printList(Node *head)
+Node *start;
+
+void printList()
 {
-    while(head != NULL){
-        cout << head->data << "\n";
-        head = head->next;
+    Node *curr = start;
+
+    while(curr != NULL)
+    {
+        cout << curr->val << " ";
+        curr = curr->next;
+    }
+
+    cout << "\n";
+}
+
+void insertAtFront(int x)
+{
+    Node *curr = new Node();
+
+    if (start == NULL){
+        curr->next = NULL;
+        curr->val = x;
+        start = curr;
+    }else{
+        curr->val = x;
+        curr->next = start;
+        start = curr;
     }
 }
 
 int main(void)
 {
-    Node *head = NULL;
-    Node *second = NULL;
+    int option;
+    int number;
+    do{
+        system("cls");
+        printList();
+        cout << "Insert\n";
+        cout << "1. Insert at front\n";
+        cout << "2. Insert at tail\n";
+        cout << "Remove\n";
+        cout << "3. Remove at front\n";
+        cout << "4. Remove at tail\n";
+        cout << "5. Search\n";
+        cout << "6. Reverse\n";
+        cout << "0. Exit\n";
+        cout << "Choose : [1-6] : ";
+        cin >> option;
 
-    head = new Node();
-    second = new Node();
+        switch(option)
+        {
+        case 1 :
+            system("cls");
+            cin >> number;
+            insertAtFront(number);
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        }
+    }while(option != 0);
 
-    head->data = 1;
-    head->next = second;
 
-    second->data = 2;
-    second->next = NULL;
 
-    printList(head);
 }
