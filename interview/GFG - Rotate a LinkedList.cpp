67d41bb6524787e5d53351bc5aebdef61251d2c6
aratnamirasugab <officialbagus15@gmail.com>
2019-10-23 15:15:23 +0700

    added GFG solution

---
+++ interview/GFG - Rotate a LinkedList.cpp
@@ -0,0 +1,53 @@
+//https://practice.geeksforgeeks.org/problems/rotate-a-linked-list/1
+
+Node* rotate(Node* head, int k) {
+    int length = 1;
+    Node *n = head;
+    while(n->next != NULL)
+    {
+        length++;
+        n = n->next;
+    }
+
+    if (k > length){
+        return head;
+    }else{
+        Node *temp = head;
+        while(k > 1)
+        {
+            temp = temp->next;
+            k--;
+        }
+        Node *newHead = temp->next;
+        Node *temp2 = temp->next;
+        temp->next = NULL;
+
+        while(temp2->next != NULL)
+        {
+            temp2 = temp2->next;
+        }
+
+        temp2->next = head;
+        head = newHead;
+        return head;
+    }
+}
+
+// another solution O(n^2)
+Node* rotate(Node* head, int k)
+{
+    for(int i=1;i<=k;i++)
+    {
+        Node *temp=head,*curr;
+        head=head->next;
+        curr=head;
+        while(curr->next!=NULL)
+        {
+            curr=curr->next;
+        }
+        curr->next=temp;
+        temp->next=NULL;
+
+    }
+    return head;
+}
