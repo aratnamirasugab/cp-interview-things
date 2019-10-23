bca114f65900152919a4ba7022ef318b888cd8f1
aratnamirasugab <officialbagus15@gmail.com>
2019-10-23 16:34:27 +0700

    unfinished solution

---
+++ interview/GFG - Reverse LL in group.cpp
@@ -0,0 +1,11 @@
+//https://practice.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1
+
+/*
+Given a linked list of size N. The task is to reverse every k nodes
+(where k is an input to the function) in the linked list.
+*/
+
+struct node *reverse (struct node *head, int k)
+{
+
+}
