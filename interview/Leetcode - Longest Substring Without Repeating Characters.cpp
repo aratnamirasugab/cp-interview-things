7120375a7cf47d323a69f95b311dfc333001d1ee
aratnamirasugab <officialbagus15@gmail.com>
2019-11-29 01:31:28 +0700

    added new approach, but still got no idea

--- interview/Leetcode - Longest Substring Without Repeating Characters.cpp
+++ interview/Leetcode - Longest Substring Without Repeating Characters.cpp
@@ -4,12 +4,14 @@
 
 #include <vector>
 #include <algorithm>
+#include <iterator>
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int resMax = 0;
         vector<int> vec;
+        vector<int> temp;
 
         for(int i = 0 ; i < s.length(); i++){
             if(find(vec.begin(), vec.end(), s[i]) == vec.end()){
@@ -18,11 +20,20 @@ public:
                 if (vec.size() > resMax){
                     resMax = vec.size(); //3
                 }
-                vec.clear();
-                if(s[i] != s[i-1]){
+
+                if(s[i] == s[0]){
+                    for(int j = 1 ; j < vec.size() ; j++){
+                        temp.push_back(vec[j]);
+                    }
+                    vec.clear();
+                    vec.assign(temp.begin(), temp.end());
+                    temp.clear();
+                }else if(s[i] != s[i-1]){
+                    vec.clear();
                     vec.push_back(s[i-1]);
                     vec.push_back(s[i]);
                 }else{
+                    vec.clear();
                     vec.push_back(s[i]);
                 }
             }
