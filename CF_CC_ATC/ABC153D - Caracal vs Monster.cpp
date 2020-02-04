d12c33f9577740fb8a21f07d5d75e35fce186be2
aratnamirasugab <officialbagus15@gmail.com>
2020-02-05 01:50:52 +0700

    added new solution

---
+++ CF_CC_ATC/ABC153D - Caracal vs Monster.cpp
@@ -0,0 +1,25 @@
+//https://atcoder.jp/contests/abc153/tasks/abc153_d
+#include <bits/stdc++.h>
+#define in cin >> 
+#define out cout <<
+#define Vi vector<long int>
+#define FOR(i,k,l) for(int i(k); i<l;i++)
+#define M 100002
+#define heck unsigned long long int
+
+using namespace std;
+
+heck rec(heck H) {
+    out("Hm");
+    if (H == 1) return 1;
+    else {
+        return 2 * rec(H/2) + 1;
+    }
+}
+
+int main(void) {
+    heck H = 0;
+    in(H);
+    
+    out(rec(H));
+}
